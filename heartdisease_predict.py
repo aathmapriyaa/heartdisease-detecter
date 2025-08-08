5b6a3867f04c20dec0784e2e5548315cd13eda01
aathmapriyaa <aathmapriyaa@gmail.com>
2025-08-08 09:36:11 +0530

    project created

---
+++ heartdisease_predict.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import sklearn.neighbors as knn 
+mydata = pd.read_csv("heart.csv") 
+x=mydata[["age","sex","cp","trestbps","chol","fbs","restecg","thalach","exang","oldpeak","slope","ca","thal"]]
+y=mydata[["target"]] 
+model=knn.KNeighborsClassifier(n_neighbors=5) 
+model.fit(x,y)
+print(model.predict([[52,1,0,125,212,0,1,168,0,1,2,2,3]])) 
