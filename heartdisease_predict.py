ffc8c357de6f049a32802059dc4499dc8cafb8ed
aathmapriyaa <aathmapriyaa@gmail.com>
2025-08-08 09:58:40 +0530

    project created

--- heartdisease_predict.py
+++ heartdisease_predict.py
@@ -5,9 +5,22 @@ x=mydata[["age","sex","cp","trestbps","chol","fbs","restecg","thalach","exang","
 y=mydata[["target"]] 
 model=knn.KNeighborsClassifier(n_neighbors=5) 
 model.fit(x,y)
-print(model.predict([[52,1,0,125,212,0,1,168,0,1,2,2,3]])) 
-target=model.predict([[52,1,0,125,212,0,1,168,0,1,2,2,3]])
-if target[0] == 1:
+getAge=int(input("Enter the age of the patient: "))
+getSex=int(input("Enter sex: ")) 
+getcp=int(input("Enter cp: ")) 
+getTrestbps=int(input("Enter Trestbps: "))
+getChol=int(input("Enter serum chol: "))
+getFbs=int(input("Enter Fbs: "))
+getRestecg=int(input("Enter Restecg: "))
+getThalach=int(input("Enter Thalach: "))
+getExang=int(input("Enter Exang: "))
+getOldpeak=float(input("Enter Oldpeak: "))
+getSlope=int(input("Enter slope: "))
+getCa=int(input("Enter Ca: "))
+getThal=int(input("Enter thal ")) 
+ 
+target=model.predict([[getAge, getSex, getcp, getTrestbps, getChol, getFbs, getRestecg, getThalach, getExang, getOldpeak, getSlope, getCa, getThal]])
+if (target[0] == 1):
     print("The patient is likely to have heart disease.")
 else:
-    print("The patient is likely to be healthy.")
+    print("The patient is likely to be healthy.") 
