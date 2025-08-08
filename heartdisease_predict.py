a1664e9bb1a958f31a1b2c7d896e56ed1b0cd228
aathmapriyaa <aathmapriyaa@gmail.com>
2025-08-08 09:37:59 +0530

    project created

--- heartdisease_predict.py
+++ heartdisease_predict.py
@@ -6,3 +6,8 @@ y=mydata[["target"]]
 model=knn.KNeighborsClassifier(n_neighbors=5) 
 model.fit(x,y)
 print(model.predict([[52,1,0,125,212,0,1,168,0,1,2,2,3]])) 
+target=model.predict([[52,1,0,125,212,0,1,168,0,1,2,2,3]])
+if target[0] == 1:
+    print("The patient is likely to have heart disease.")
+else:
+    print("The patient is likely to be healthy.")
